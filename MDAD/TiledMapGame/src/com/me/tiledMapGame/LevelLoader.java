1ec21bb2d6902ffb47ee4c5110d561e9b6c8ef42
bwj-GitHub <bwj_ucf@knights.ucf.edu>
2013-11-17 19:52:00 -0500

    Added Class to load levels (unfinished)

---
+++ MDAD/TiledMapGame/src/com/me/tiledMapGame/LevelLoader.java
@@ -0,0 +1,36 @@
+package com.me.tiledMapGame;
+/**
+ * Opens a text file containing save information from a previous game and restores that game.
+ * 
+ * @author Brandon
+ *
+ */
+public class LevelLoader {
+	private Level l;
+	private String checkSum; //Used to make sure that the save file is not tampered with.
+	
+	public LevelLoader(){
+		l = null;
+	}
+	
+	/**
+	 * Open text file named in 'loadFile' and create a level based on the information read.
+	 * Checks if the 'checkSum' is valid, if not, the level will not be loaded.
+	 * 
+	 * @param loadFile Name of file to be loaded in form 'name.txt'.
+	 * @return True if succesful; false if the level is not found or the 'checkSum' is not valid.
+	 */
+	public boolean loadLevel(String loadFile){
+		//TODO
+	
+		return false;
+	}
+	/**
+	 * Retrieves the level created by 'loadLevel()'. Assumes 'loadLevel()' returned true and was previously ran.
+	 * @return The level created by 'loadLevel()'.
+	 */
+	public Level getLevel(){
+		return l;
+	}
+
+}
