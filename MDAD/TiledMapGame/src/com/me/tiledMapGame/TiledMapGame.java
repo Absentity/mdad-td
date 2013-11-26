3305eae07c807ff0855eb8cc873e1549685f4c1c
reggie-fergerson <rlferggy@hotmail.com>
2013-11-25 21:45:28 -0500

    added credits screen

--- MDAD/TiledMapGame/src/com/me/tiledMapGame/TiledMapGame.java
+++ MDAD/TiledMapGame/src/com/me/tiledMapGame/TiledMapGame.java
@@ -3,6 +3,7 @@ package com.me.tiledMapGame;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
@@ -186,12 +187,23 @@ public class TiledMapGame extends Game {
 	@Override
 	public void render() {		
 		super.render();
+		
+		if(this.getScreen() == C) {
+			if(Gdx.input.isKeyPressed(Keys.ESCAPE)) {
+				System.out.println("here");
+				M = new MainMenuScreen();
+				this.setScreen(M);
+				C.dispose();
+			}
+		}
+		
 		if(M == null)
 			M = new MainMenuScreen();
 		
 		if(M.creditsMenuListener.isPressed()) {
 			M.dispose();
 			C = new CreditsScreen();
+			this.setScreen(C);
 		}
 		
 		//Set the screen to the level select screen
--- MDAD/TiledMapGame/src/com/me/tiledMapGame/screens/CreditsScreen.java
+++ MDAD/TiledMapGame/src/com/me/tiledMapGame/screens/CreditsScreen.java
@@ -2,21 +2,37 @@ package com.me.tiledMapGame.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.me.tiledMapGame.TiledMapGame;
+import com.me.tiledMapGame.entities.Enemy;
+import com.me.tiledMapGame.entities.EnemyType;
 
 public class CreditsScreen implements Screen {
 
 	private SpriteBatch sb;
+	private Texture t = new Texture("img/Credits.png");
+	private MainMenuScreen M;
+	private Enemy pic = new Enemy(TiledMapGame.enemyTypeLibrary.get("Wyvern"), true);
 	
 	@Override
 	public void render(float delta) {
 		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		sb.begin();
+		sb.draw(t, 0, 0);
+		pic.update(Gdx.graphics.getDeltaTime());
+		if(!pic.getCurrentFrame().isFlipX()) {
+			pic.getCurrentFrame().flip(true, false);
+		}
+		sb.draw(pic.getCurrentFrame(), 430, 450-32);
+		sb.end();
 		
 	}
 
